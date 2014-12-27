2f5ef84e86260cbf0b7e9dd8b7d5b7db4fe0545e
Ciel <imwithye@gmail.com>
2014-12-27 22:11:45 +0800

    doc of the csv parser

--- src/GaLG/parser/parser.h
+++ src/GaLG/parser/parser.h
@@ -5,6 +5,19 @@
 
 namespace GaLG {
   namespace parser {
+    /**
+     * @brief Parsing the csv file.
+     * @details Reading data from the csv file and
+     *          store the information as a raw_data
+     *          instance.
+     * 
+     * @param file The path to the csv file.
+     * @param data The reference to the raw_data instance.
+     *             The data in this instance will be replaced
+     *             by the data read from the csv file.
+     * 
+     * @return Lines of csv file.
+     */
     int csv(string file, raw_data& data);
   }
 }
