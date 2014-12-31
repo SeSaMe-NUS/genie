744579fcf3f31fd310e7daed5e3c596a4dbbe9ea
Ciel <imwithye@gmail.com>
2014-12-31 14:49:20 +0800

    add doc of query and match

--- src/GaLG/match.h
+++ src/GaLG/match.h
@@ -11,9 +11,23 @@ using namespace thrust;
 
 namespace GaLG
 {
+  /**
+   * @brief Search the inv_table and save the match
+   *        result into d_count and d_aggregation.
+   * @details Search the inv_table and save the match
+   *          result into d_count and d_aggregation.
+   * 
+   * @param table The inv_table which will be searched.
+   * @param queries The quries.
+   * @param d_count The count result.
+   * @param d_aggregation The aggregation result.
+   * 
+   * @throw inv_table::not_builded_exception if the table has not been builded.
+   * @throw inv_table::not_matched_exception if the query is not querying the given table.
+   */
   void
   match(inv_table& table, vector<query>& queries, device_vector<int>& d_count,
-      device_vector<float>& d_agg) throw (int);
+      device_vector<float>& d_aggregation) throw (int);
 }
 
 #endif
--- src/GaLG/query.h
+++ src/GaLG/query.h
@@ -19,25 +19,70 @@ namespace GaLG
       int up;
       float weight;
     };
+
   private:
     inv_table* _ref_table;
     vector<dim> _attr;
     vector<dim> _dims;
 
   public:
+    /**
+     * @brief Create a query based on a inv_table.
+     * @details Create a query based on a inv_table.
+     * 
+     * @param ref The refrence to the inv_table.
+     */
     query(inv_table* ref);
+
+    /**
+     * @brief Create a query based on a inv_table.
+     * @details Create a query based on a inv_table.
+     * 
+     * @param ref The pointer to the inv_table.
+     */
     query(inv_table& ref);
+
+    /**
+     * @brief The refrence table's pointer.
+     * @details The refrence table's pointer.
+     * @return The pointer points to the refrence table.
+     */
     inv_table*
     ref_table();
+
+    /**
+     * @brief Modify the matching range and weight of an attribute.
+     * @details Modify the matching range and weight of an attribute.
+     * 
+     * @param index Attribute index
+     * @param low Lower bound
+     * @param up Upper bound
+     * @param weight The weight
+     */
     void
     attr(int index, int low, int up, float weight);
 
+    /**
+     * @brief Construct the query based on a normal table.
+     * @details Construct the query based on a normal table.
+     */
     void
     build();
 
+    /**
+     * @brief Construct the query based on a compressed table.
+     * @details Construct the query based on a compressed table.
+     */
     void
     build_compressed();
 
+    /**
+     * @brief Transfer matching information to vector vout.
+     * @details Transfer matching information to vector vout. vout will not
+     *          be cleared, the push_back method will be invoked instead.
+     * 
+     * @param vout The target vector.
+     */
     void
     dump(vector<dim>& vout);
   };
