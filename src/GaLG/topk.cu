103612cc1688c42cf0fdf721dd11aeebd89affe9
Ciel <imwithye@gmail.com>
2015-01-04 20:32:01 +0800

    topk for int vector

--- src/GaLG/topk.cu
+++ src/GaLG/topk.cu
@@ -1,14 +1,53 @@
 #include "topk.h"
 
-struct ValueOf
+struct ValueOfFloat
 {
-  float max;
-  __host__ __device__ float
+  float max;__host__ __device__ float
   valueOf(float data)
   {
     return (float) max - data;
   }
 };
+
+struct ValueOfInt
+{
+  float max;__host__ __device__ float
+  valueOf(int data)
+  {
+    return (float) max - data;
+  }
+};
+
+void
+topk(device_vector<int>& d_search, device_vector<int>& d_tops,
+    device_vector<int>& d_top_indexes)
+{
+  int parts = d_tops.size();
+  int total = 0, i, num;
+  for (i = 0; i < parts; i++)
+    {
+      num = d_tops[i];
+      total += num;
+    }
+  thrust::pair<device_vector<int>::iterator, device_vector<int>::iterator> minmax =
+      thrust::minmax_element(d_search.begin(), d_search.end());
+  host_vector<int> h_end_index(parts);
+  device_vector<int> d_end_index(parts);
+  int number_of_each = d_search.size() / parts;
+  for (i = 0; i < parts; i++)
+    {
+      h_end_index[i] = (i + 1) * number_of_each;
+    }
+  d_end_index = h_end_index;
+  d_top_indexes.clear(), d_top_indexes.resize(total);
+
+  ValueOfInt val;
+  val.max = *minmax.second;
+  float min = *minmax.first;
+  float max = *minmax.second;
+  bucket_topk<int, ValueOfInt>(&d_search, val, min, max, &d_tops, &d_end_index,
+      parts, &d_top_indexes);
+}
 
 void
 GaLG::topk(device_vector<float>& d_search, device_vector<int>& d_tops,
@@ -33,8 +72,8 @@ GaLG::topk(device_vector<float>& d_search, device_vector<int>& d_tops,
   d_end_index = h_end_index;
   d_top_indexes.clear(), d_top_indexes.resize(total);
 
-  ValueOf val;
+  ValueOfFloat val;
   val.max = *minmax.second;
-  bucket_topk<float, ValueOf>(&d_search, val, *minmax.first, *minmax.second,
-      &d_tops, &d_end_index, parts, &d_top_indexes);
+  bucket_topk<float, ValueOfFloat>(&d_search, val, *minmax.first,
+      *minmax.second, &d_tops, &d_end_index, parts, &d_top_indexes);
 }
--- src/GaLG/topk.h
+++ src/GaLG/topk.h
@@ -12,6 +12,18 @@ namespace GaLG
   /**
    * @brief Find the top k values in given device_vector.
    * @details Find the top k values in given device_vector.
+   *
+   * @param d_search The data vector
+   * @param d_tops The top k values.
+   * @param d_top_indexes The results' indexes.
+   */
+  void
+  topk(device_vector<float>& d_search, device_vector<int>& d_tops,
+      device_vector<int>& d_top_indexes);
+
+  /**
+   * @brief Find the top k values in given device_vector.
+   * @details Find the top k values in given device_vector.
    * 
    * @param d_search The data vector
    * @param d_tops The top k values.
