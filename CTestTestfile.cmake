eceea133b0ca70c99c547914a2f086570769b2f3
LuanWH <lwhluan@gmail.com>
2015-07-03 17:23:04 +0800

    Delete mistakenly uploaded files

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/hd1/home/luanwenhao/sesame/libGaLG_old/libGaLG
-# Build directory: /media/hd1/home/luanwenhao/sesame/libGaLG_old/libGaLG
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
-SUBDIRS(test)
