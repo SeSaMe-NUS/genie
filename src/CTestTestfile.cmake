b36d11e45c86ff5d64764236f97123523e2be9ad
zhoujingbo <jzhou@comp.nus.edu.sg>
2015-11-24 15:18:54 +0800

    rm un-used makefile

--- src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/hd1/home/luanwenhao/sesame/libGaLG_old/libGaLG/src
-# Build directory: /media/hd1/home/luanwenhao/sesame/libGaLG_old/libGaLG/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
