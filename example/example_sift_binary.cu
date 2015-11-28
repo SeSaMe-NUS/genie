18d31014283bbbe621041a54469580e348ffca0b
yangyueji <dempseyyang@gmail.com>
2015-11-28 18:49:32 +0800

    Added binary reading style and the function to help convert a csvfile to binaryfile

---
+++ example/example_sift_binary.cu
@@ -0,0 +1,182 @@
+/**
+*example clean and format by jingbo
+description: create a running example fo the library. 
+2015.09.10
+*/
+
+#include "GPUGenie.h" //for ide: change from "GPUGenie.h" to "../src/GPUGenie.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace GPUGenie;
+using namespace std;
+
+
+
+int main(int argc, char * argv[])
+{
+	std::vector<std::vector<int> > queries;
+	//std::vector<std::vector<int> > data;
+	inv_table table;
+    GPUGenie::GPUGenie_Config config;
+	//Reading file from the disk. Alternatively, one can simply use vectors generated from other functions
+	//Example vectors:
+	//Properties: 10 points, 5 dimensions, value range 0-255, -1 for excluded dimensions.
+	//|id|dim0|dim1|dim2|dim3|dim4|
+	//|0	|2	 |255 |16  |0   |-1  |
+	//|1	|10	 |-1  |52  |62  |0   |
+	//|...  |... |... |... |... |... |
+	//|9	|0   |50  |253 |1   |164 |
+
+	int queryNum = 10;
+	string  dataFile = "sift_1k.dat";//for ide: from "sift_1k.csv" to "example/sift_1k.csv"
+    string  queryFile= "sift_1k.csv";   
+
+	//read_file(data, dataFile.c_str(), -1);//for AT: for adaptiveThreshold
+	read_file(dataFile.c_str(), &config.data, config.item_num, &config.index, config.row_num);
+	
+	//read queries from file, which has the same format 
+	read_file(queries, queryFile.c_str(), queryNum);
+	cout<<config.data[0]<<endl;
+	cout<<config.data[config.item_num-1]<<endl;
+
+	/*** Configuration of KNN Search ***/
+	//GPUGenie::GPUGenie_Config config;
+
+	//Data dimension
+	config.dim = 128;
+
+	//Points with dim counts lower than threshold will be discarded and not shown in topk.
+	//It is implemented as a bitmap filter.
+	//Set to 0 to disable the feature.
+	//set to <0, to use adaptiveThreshold, the absolute value of count_threshold is the maximum possible count sotred in the bitmap
+	config.count_threshold = -128;
+
+	//Number of topk items desired for each query.
+	//Some queries may result in fewer than desired topk items.
+	config.num_of_topk = 100;
+
+	//if config.hashtable_size<=2, the hashtable_size means ratio against data size
+		//Hash Table size is set as: config.hashtable_size (i.e.) ratio X data size.
+		//Topk items will be generated from the hash table so it must be sufficiently large.
+		//If set too small, the program will attempt to increase the size by 0.1f as many times
+		//as possible. So to reduce the attempt time waste, please set to 1.0f if memory allows.
+	//if config.hashtable_size>2, the hashtable_size means the size of the hashtable,
+		//this is useful when using adaptiveThreshold (i.e. config.count_threshold <0), where the
+		//hash_table size is usually set as: maximum_countXconfig.num_of_topkx1.5 (where 1.5 is load factor for hashtable).
+	config.hashtable_size = 128*config.num_of_topk*1.5;//960
+
+
+
+
+	//Query radius from the data point bucket expanding to upward and downward.
+	//Will be overwritten by selectivity if use_adaptive_range is set.
+	config.query_radius = 1;
+
+	//Index of the GPU device to be used. If you only have one card, then set to 0.
+	config.use_device = 0;
+
+	//Number of hot dimensions with long posting lists to be avoided.
+	//Once set to n, top n hot dimensions will be split from the query and submit again
+	//at the second stage. Set to 0 to disable the feature.
+	//May reduce hash table usage and memory usage.
+	//Will increase time consumption.
+	config.num_of_hot_dims = 0;
+
+	//Threshold for second stage hot dimension scan. Points with counts lower than threshold
+	//will not be processed and they will not be present in the hash table.
+	//The value should be larger than count_threshold.
+	config.hot_dim_threshold = 0;//not useful
+
+	//Set if adaptive range of query is used.
+	//Once set with a valid selectivity, the query will be re-scanned to
+	//guarantee at least (selectivity * data size) of the data points will be matched
+	//for each dimension.
+	config.use_adaptive_range = false;
+
+	//The selectivity to be used. Range 0.0f (no other bucket to be matched) to 1.0f (match all buckets).
+
+	config.selectivity = 0.2f;
+
+	
+	//The pointer to the vector containing the data.
+	//config.data_points = &_data;
+
+	//The pointer to the vector containing the queries.
+	config.query_points = &queries;
+
+	config.multiplier = 1.5f;
+	config.posting_list_max_length = 6400;
+	config.use_load_balance = false;
+	/*** End of Configuration ***/
+
+	/*** NOTE TO DEVELOPERS ***/
+	/*
+	 The library is still under development. Therefore there might be crashes if
+	 the parameters are not set optimally.
+
+	 Optimal settings may help you launch ten times more queries than naive setting,
+	 but reducing hash table size may result in unexpected crashes or insufficient
+	 topk results. (How can you expect 100 topk results from a hash table of size 50
+	 or a threshold of 100 suppose you only have 128 dimensions?) So please be careful
+	 if you decide to use non-default settings.
+
+	 The basic & must-have settings are:
+	 1. dim
+	 2. num_of_topk
+	 3. data_points
+	 4. query_points
+	 And leave the rest to default.
+
+	 Recommended settings:
+	 If you want to increase the query selectivity (default is one bucket itself, i.e. radius 0,
+	 which is absolutely not enough), you can use either way as follows:
+	 A. set radius to a larger value, e.g. 1, 2, 3 and so on. It will expand your search
+	 	 upwards and downward by the amount of buckets you set. However, it does not guarantee
+	 	 the selectivity since data distribution may not even.
+	 B. set the selectivity and turn on use_adaptive_range, e.g. selectivity = 0.01 and
+	 	 use_adaptive_range = true. It can guarantee that on each dimension the query will match
+	 	 for at least (selectivity * data size) number of data points.
+	 Note that approach B will overwrite approach A if both are set.
+
+	 Advanced settings:
+	 For num_of_hot_dims and hot_dim_threshold, please contact the author for details.
+
+	 Author: Luan Wenhao
+	 Contact: lwhluan@gmail.com
+	 Date: 24/08/2015
+	                          */
+	/*** END OF NOTE ***/
+
+	std::vector<int> result;
+
+
+    cout<<config.item_num<<" "<<config.row_num<<endl;
+
+
+	printf("Launching knn functions...\n");
+	u64 start = getTime();
+	GPUGenie::knn_search(result, config);
+	u64 end = getTime();
+	double elapsed = getInterval(start, end);
+	printf(">>>>>>> [time profiling]: Total Time Elapsed: %fms. <<<<<<<\n", elapsed);
+
+
+	for(int i = 0; i < 5; ++i)
+
+	{
+		printf("Query %d result is: \n\t", i);
+		for (int j = 0; j < 5; ++j)
+		{
+			printf("%d, ", result[i * config.num_of_topk + j]);
+		}
+		printf("\n");
+	}
+	
+	free(config.data);
+	free(config.index);
+	return 0;
+}
---
+++ example/example_tweets_binary.cu
@@ -0,0 +1,111 @@
+/*
+ * example_tweets.cu
+ *
+ *  Created on: Oct 16, 2015
+ *      Author: zhoujingbo
+ *
+ * description: This program is to demonstrate the search on string-like data by the GPU. More description of the parameter configuration please refer to example.cu file
+ */
+
+#include "GPUGenie.h" //for ide: change from "GPUGenie.h" to "../src/GPUGenie.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace GPUGenie;
+using namespace std;
+
+
+int main(int argc, char * argv[])//for ide: from main to main4
+{
+	std::vector<std::vector<int> > queries;
+	//std::vector<std::vector<int> > data;
+	inv_table table;
+    GPUGenie::GPUGenie_Config config;
+	read_file("tweets_4k.dat", &config.data, config.item_num, &config.index, config.row_num);//for debug
+	read_file(queries, "tweets_4k.csv", 100);//for debug
+
+	//Data dimension
+	//For string search, we use one-dimension-mulitple-values method,
+	// i.e. there is only one dimension, all words are considered as the values. 
+	//given a query, there can be multiple values for this dimension. 
+	//This is like a bag-of-word model for string search
+	config.dim = 14;
+
+	//Points with dim counts lower than threshold will be discarded and not shown in topk.
+	//It is implemented as a bitmap filter.
+	//Set to 0 to disable the feature.
+	//set to <0, to use adaptiveThreshold, the absolute value of count_threshold is the maximum possible count sotred in the bitmap
+	config.count_threshold = -14;
+
+	//Number of topk items desired for each query.
+	config.num_of_topk = 100;
+
+	//if config.hashtable_size<=2, the hashtable_size means ratio against data size
+			//Hash Table size is set as: config.hashtable_size (i.e.) ratio X data size.
+			//Topk items will be generated from the hash table so it must be sufficiently large.
+			//If set too small, the program will attempt to increase the size by 0.1f as many times
+			//as possible. So to reduce the attempt time waste, please set to 1.0f if memory allows.
+	//if config.hashtable_size>2, the hashtable_size means the size of the hashtable,
+			//this is useful when using adaptiveThreshold (i.e. config.count_threshold <0), where the
+			//hash_table size is usually set as: maximum_countXconfig.num_of_topkx1.5 (where 1.5 is load factor for hashtable).
+	config.hashtable_size = 14*config.num_of_topk*1.5;//960
+
+	//Query radius from the data point bucket expanding to upward and downward.
+		//For tweets data, set it as 0, which means exact match
+	//Will be overwritten by selectivity if use_adaptive_range is set.
+	config.query_radius = 0;
+
+	//Index of the GPU device to be used. If you only have one card, then set to 0.
+	config.use_device = 0;
+
+	config.num_of_hot_dims = 0;//not useful
+	config.hot_dim_threshold = 0;//not useful
+
+	//use_adaptive_range is not suitable for string search
+	config.use_adaptive_range = false;
+	config.selectivity = 0.0f;
+
+	config.query_points = &queries;
+
+	//if use_load_balance=false, config.multiplier and config.posting_list_max_length are not useful
+	config.use_load_balance = true;
+	//maximum number per posting list, if a keyword has a long posting list, we break it into sublists, and this parameter defines the maximum length of sub-list
+	config.posting_list_max_length = 64000;
+	config.multiplier = 1.5f;//config.multiplier*config.posting_list_max_length is  maximum number of elements processed by one block
+
+	std::vector<int> result;
+	printf("Launching knn functions...\n");
+	u64 start = getTime();
+
+	/**
+	* @brief Search on the inverted index and save the result in result
+	* bijectMap means building each ordered pair/keyword is also transformed by a bijection map. (Different from the default method, where the
+	* keyword is a combination of dimension and value
+	* Previous name: knn_search_tweets()
+	*
+	*/
+	GPUGenie::knn_search_bijectMap(result, config);
+	u64 end = getTime();
+	double elapsed = getInterval(start, end);
+	printf(">>>>>>>[time profiling]: Total Time Elapsed: %fms. <<<<<<<\n", elapsed);
+
+	for(int i = 0; i < 10 && i < queries.size(); ++i)
+	{
+		printf("Query %d result is: \n\t", i);
+		for (int j = 0; j < 10; ++j)
+		{
+			printf("%d, ", result[i * config.num_of_topk + j]);
+		}
+		printf("\n");
+	}
+    
+    free(config.data);
+    free(config.index);
+
+    return 0;
+}
+
+
