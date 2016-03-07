1f8f4fd0804db51d8ba1f3bfa45c7b2265d487dd
LuanWH <lwhluan@gmail.com>
2016-03-07 13:25:18 +0800

    Revise documentation of match.h

--- src/GPUGenie/match.h
+++ src/GPUGenie/match.h
@@ -25,7 +25,8 @@ typedef uint32_t u32;
 typedef unsigned long long u64;
 
 /*! \struct data_
- *  \brief This is the entry of hashtable used in GPU.
+ *  \brief This is the entry format of the hash table used in GPU.
+ *  	   Will be treated as a 64-bit unsigned integer later.
  */
 
 /*! \typedef struct data_ data_t
@@ -60,38 +61,29 @@ cal_max_topk(vector<query>& queries);
  *
  *  \param table The inv_table which will be searched.
  *  \param queries The quries.
- *  \param d_data The output data consisting of count, aggregation
- *               and the index of the data in big table.
+ *  \param d_data The output data consisting of count and the index of the data in table.
  *  \param hash_table_size The hash table size.
- *  \param max_load The maximum length of posting list that can be processed by one gpu block
- *  \param bitmap_bits The threshold value for one data point
+ *  \param max_load The maximum number of posting list items that can be processed by one gpu block
+ *  \param bitmap_bits The threshold for the count heap
  *  \param d_noiih The number of items in hash table
- *
- *  This functions throw two exceptions that may be caught.
- *  throw inv_table::not_builded_exception if the table has not been builded.
- *  throw inv_table::not_matched_exception if the query is not querying the given table.
  */
 void
 match(inv_table& table, vector<query>& queries, device_vector<data_t>& d_data,
 		int hash_table_size, int max_load, int bitmap_bits,
 		device_vector<u32>& d_noiih);
+
 /*! \fn void match(inv_table& table, vector<query>& queries, device_vector<data_t>& d_data, device_vector<u32>& d_bitmap,int hash_table_size,
  *          int max_load, int bitmap_bits, device_vector<u32>& d_noiih, device_vector<u32> d_threshold, device_vector<u32>& d_passCount)
  *  \brief Search the inv_table and save the match
  *        result into d_count and d_aggregation.
  *
  *  \param table The inv_table which will be searched.
- *  \param queries The quries.
- *  \param d_data The output data consisting of count, aggregation
- *               and the index of the data in big table.
+ *  \param queries The queries.
+ *  \param d_data The output data consisting of count and the index of the data in table.
  *  \param hash_table_size The hash table size.
- *  \param max_load The maximum length of posting list that can be processed by one gpu block
- *  \param bitmap_bits The threshold value for one data point
+ *  \param max_load The maximum number of posting list items that can be processed by one gpu block
+ *  \param bitmap_bits The threshold for the count heap
  *  \param d_noiih The number of items in hash table
- *
- *  This functions throw two exceptions that may be caught.
- *  throw inv_table::not_builded_exception if the table has not been builded.
- *  throw inv_table::not_matched_exception if the query is not querying the given table.
  */
 void
 match(inv_table& table, vector<query>& queries, device_vector<data_t>& d_data,
@@ -104,11 +96,11 @@ match(inv_table& table, vector<query>& queries, device_vector<data_t>& d_data,
  *  \brief Collect all the dims in all queries.
  *
  *  \param quereis The query set
- *  \param table The given inverted table.
- *  \param dims The dim set of all dims, to be returned.
- *  \param max_load Maximum length of posting list processed by one gpu block.
+ *  \param table The inverted table.
+ *  \param dims The container for the resulting query details.
+ *  \param max_load The maximum number of posting list items that can be processed by one gpu block
  *
- *  \return The max count of queries in the query set.
+ *  \return The max value of counts of queries in the query set.
  */
 int
 build_queries(vector<query>& queries, inv_table& table,
