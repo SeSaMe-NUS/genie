4b65983e38e7bc7311f9a515d17ffbc78a875864
Siyuan Liu <weifengzi2009@gmail.com>
2017-03-28 15:49:50 +0800

    fix Logger::set_logfile_name bug

--- src/GPUGenie/Logger.cc
+++ src/GPUGenie/Logger.cc
@@ -73,14 +73,9 @@ void Logger::set_logfile_name(const char * name)
 {
 	if (strcmp(name, _logger()->logfile_name) != 0)
 	{
+		fclose(_logger()->logfile); // close previously opened logger file
 		strcpy(_logger()->logfile_name, name);
-		if (logger != NULL)
-		{
-			fclose(logger->logfile);
-			logger = NULL;
-			_logger();
-		}
-
+		fopen(_logger()->logfile_name, "a"); // open a new file for logging
 	}
 
 }
