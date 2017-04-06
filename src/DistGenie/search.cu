af65201aa1837253b94fb0306e3fd45cff80067e
Siyuan Liu <weifengzi2009@gmail.com>
2017-04-06 15:56:31 +0800

    add openmp, add copy constructor for GPUGenie_Config

--- src/DistGenie/search.cu
+++ src/DistGenie/search.cu
@@ -45,38 +45,13 @@ void ExecuteQuery(GPUGenie_Config &config, ExtraConfig &extra_config, inv_table 
 	MPI_Gather(&result_count[0], single_rank_result_size, MPI_INT, final_result_count, single_rank_result_size, MPI_INT, 0, MPI_COMM_WORLD);
 
 	if (g_mpi_rank == 0) {
-		//vector<int> final_result_vec;
-		//vector<int> final_result_count_vec;
-
-		// merge results for each query
-		for (int i = 0; i < config.num_of_queries; ++i) {
-			// count is first, id is second (sort by count)
-			//priority_queue<pii, vector<pii>, std::less<pii> > single_query_priority_queue;
+		// add result from a single cluster to results vector
+		for (int i = 0; i < config.num_of_queries; ++i)
 			for (int j = 0; j < g_mpi_size; ++j) {
 				int offset = j * single_rank_result_size + i * config.num_of_topk;
-				for (int k = 0; k < config.num_of_topk; ++k) {
-					// k-th result on j-th rank for i-th query
-					//single_query_priority_queue.push(pii(final_result_count[offset + k], final_result[offset + k]));
+				for (int k = 0; k < config.num_of_topk; ++k)
 					results.at(query_id.at(i)).push_back(pii(final_result_count[offset + k], final_result[offset + k]));
-				}
 			}
-
-			// append top k of i-th query to final result vector (which contains top k of all queries)
-			//for (int j = 0; j < config.num_of_topk; ++j) {
-			//	pii single_result = single_query_priority_queue.top();
-			//	results.at(i).push_back(single_result);
-			//	single_query_priority_queue.pop();
-			//	final_result_vec.push_back(single_result.second);
-			//	final_result_count_vec.push_back(single_result.first);
-			//}
-		}
-
-		// write result to file
-		//ofstream output(extra_config.output_file);
-		//for (auto it1 = final_result_vec.begin(), it2 = final_result_count_vec.begin(); it1 != final_result_vec.end(); ++it1, ++it2) {
-		//	output << *it1 << "," << *it2 << endl;
-		//}
-		//output.close();
 
 		// cleanup
 		delete[] final_result;
@@ -88,7 +63,7 @@ void ExecuteQuery(GPUGenie_Config &config, ExtraConfig &extra_config, inv_table 
 void ExecuteMultitableQuery(GPUGenie::GPUGenie_Config &config, ExtraConfig &extra_config,
 		GPUGenie::inv_table **tables, vector<Cluster> &clusters, vector<Result> &results)
 {
-	for (size_t i = 0; i < clusters.size(); ++i)
+	for (vector<Cluster>::size_type i = 0; i < clusters.size(); ++i)
 	{
 		clog << MPI_DEBUG << g_mpi_rank << " searching cluster " << i << endl;
 		config.num_of_queries = clusters.at(i).m_queries.size();
