2ae97c6976e2cc7cffc8a8046070d62c484355e6
Lubos Krcal <lubos.krcal@gmail.com>
2017-05-03 11:18:27 +0800

    Improve CompositeCodec to use extra hint for determining end of the first codec

--- src/GPUGenie/DeviceCompositeCodec.cu
+++ src/GPUGenie/DeviceCompositeCodec.cu
@@ -142,111 +142,122 @@ GPUGenie::DeviceCompositeCodec<Codec1,Codec2>::encodeArray(uint32_t *in, const s
     assert(length > 0);
     assert(nvalue > 0);
     int codec1minEffLength = codec1.decodeArrayParallel_minEffectiveLength();
-    const size_t codec1Length = length / codec1minEffLength * codec1minEffLength;
-    size_t nvalue1 = nvalue;
+    size_t codec1Length = (length / codec1minEffLength) * codec1minEffLength;
+    size_t codec2Length = length - codec1Length;
+    assert (codec1Length + codec2Length == length);
+    assert (codec2Length <= length);
+
+    size_t nvalue1 = 0;
     if (codec1Length){
-        codec1.encodeArray(in, codec1Length, out, nvalue1);
-    } else {
-        nvalue1 = 0;
+        nvalue1 = nvalue;
+        codec1.encodeArray(in, codec1Length, out + 1, nvalue1);
+        assert(nvalue >= nvalue1); // Error - compression overflow
     }
-    if (codec1Length < length) {
-        assert(nvalue >= nvalue1);
-        size_t nvalue2 = nvalue - nvalue1;
-        codec2.encodeArray(in + codec1Length, length - codec1Length, out + nvalue1, nvalue2);
-        nvalue = nvalue1 + nvalue2;
-    } else {
-        nvalue = nvalue1;
+
+    size_t nvalue2 = 0;
+    if (codec2Length) {
+        nvalue2 = nvalue - nvalue1;
+        codec2.encodeArray(in + codec1Length, codec2Length, out + 1 + nvalue1, nvalue2);
+        assert(nvalue - nvalue1 >= nvalue2); // Error - compression overflow
     }
+
+    out[0] = nvalue1; // store infromation about compressed length from the first codec
+
+    nvalue = 1 + nvalue1 + nvalue2;
 }
 
 template <class Codec1, class Codec2> const uint32_t*
-GPUGenie::DeviceCompositeCodec<Codec1,Codec2>::decodeArray(const uint32_t *in, const size_t length, uint32_t *out, size_t &nvalue)
+GPUGenie::DeviceCompositeCodec<Codec1,Codec2>::decodeArray(const uint32_t *in, const size_t comprLength, uint32_t *out, size_t &nvalue)
 {
-    const uint32_t *const initin(in);
-    size_t mynvalue1 = nvalue;
-    const uint32_t *in2 = codec1.decodeArray(in, length, out, mynvalue1);
-    if (length + in > in2) {
-        assert(nvalue > mynvalue1);
-        size_t nvalue2 = nvalue - mynvalue1;
-        const uint32_t *in3 = codec2.decodeArray(in2, length - (in2 - in), out + mynvalue1, nvalue2);
-        nvalue = mynvalue1 + nvalue2;
-        assert(initin + length >= in3);
-        return in3;
+    size_t firstCodecComprLength = *in++;
+
+    // Codec1 decompresses as much as it can
+    size_t nvalue1 = 0;
+    const uint32_t *inForCodec2 = in;
+
+    if (firstCodecComprLength){
+        nvalue1 = nvalue; // set capacity for codec1 to overall capacity
+        inForCodec2 = codec1.decodeArray(in, firstCodecComprLength, out, nvalue1);
+
+        if (nvalue1 > nvalue){ // Error - Codec1 does not have enough capacity
+            nvalue = nvalue1; // Set nvalue to required capacity of codec1
+            return in; // Return pointer to the deginning of the compressed array
+        }
+
+        if (inForCodec2 == in + comprLength - 1){ // Codec1 decompressed everything
+            nvalue = nvalue1;
+            return inForCodec2;
+        }
     }
-    nvalue = mynvalue1;
-    assert(initin + length >= in2);
-    return in2;
+
+    assert(inForCodec2 == in + firstCodecComprLength); // Make sure codec1 returned correct d_in pointer
+
+    // Codec2 decompresses the leftover
+    size_t nvalue2 = nvalue - nvalue1; // remaining capacity
+    size_t leftoverLength = comprLength - 1 - (inForCodec2 - in);
+    const uint32_t *inAfterBothCodecs = codec2.decodeArray(inForCodec2, leftoverLength, out + nvalue1, nvalue2);
+
+    if (nvalue2 > nvalue - nvalue1){ // Error - Codec2 does not have enough capacity
+        nvalue = nvalue1 + nvalue2; // Set nvalue to required capacity of codec1 + codec2
+        return in; // Return pointer to the deginning of the compressed array
+    }
+
+    assert(in + comprLength - 1 == inAfterBothCodecs);
+    nvalue = nvalue1 + nvalue2;
+    return inAfterBothCodecs;
 }
+
 
 template <class Codec1, class Codec2> __device__ uint32_t*
 GPUGenie::DeviceCompositeCodec<Codec1,Codec2>::decodeArraySequential(uint32_t *d_in, size_t length, uint32_t *d_out, size_t &nvalue)
 {
-    uint32_t *initin(d_in), *d_in2(d_in);
-    int codec1minEffLength = codec1.decodeArrayParallel_minEffectiveLength();
-    const size_t codec1Length = length / codec1minEffLength * codec1minEffLength;
-    size_t mynvalue1 = nvalue;
-    if (codec1Length){
-        d_in2 = codec1.decodeArraySequential(d_in, length, d_out, mynvalue1);
-        if (mynvalue1 > nvalue){ // Codec1 does not have enough capacity
-            nvalue = mynvalue1;
-            return d_in;
-        }
-    }
-    if (length + d_in > d_in2) {
-        assert(nvalue > mynvalue1);
-        size_t nvalue2 = nvalue - mynvalue1;
-        uint32_t *in3 = codec2.decodeArraySequential(d_in2, length - (d_in2 - d_in), d_out + mynvalue1, nvalue2);
-        if (nvalue2 > nvalue - mynvalue1){ // Codec2 does not have enough capacity
-            nvalue = mynvalue1 + nvalue2;
-            return d_in;
-        }
-        nvalue = mynvalue1 + nvalue2;
-        assert(initin + length >= in3);
-        return in3;
-    }
-    nvalue = mynvalue1;
-    assert(initin + length >= d_in2);
-    return d_in2;
+    return nullptr;
 }
 
-template <class Codec1, class Codec2> __device__ uint32_t*
-GPUGenie::DeviceCompositeCodec<Codec1,Codec2>::decodeArrayParallel(uint32_t *d_in, size_t length, uint32_t *d_out, size_t &nvalue)
-{
-    uint32_t *initin(d_in), *d_in2(d_in);
-    int codec1minEffLength = codec1.decodeArrayParallel_minEffectiveLength();
-    const size_t codec1Length = length / codec1minEffLength * codec1minEffLength;
-    size_t nvalue1 = nvalue;
 
-    // Codec1 decompressed as much as it can
-    if (codec1Length){
-        d_in2 = codec1.decodeArrayParallel(d_in, length, d_out, nvalue1);
+
+template <class Codec1, class Codec2> __device__ uint32_t*
+GPUGenie::DeviceCompositeCodec<Codec1,Codec2>::decodeArrayParallel(
+            uint32_t *d_in, size_t comprLength, uint32_t *d_out, size_t &nvalue)
+{
+    size_t firstCodecComprLength = *d_in++;
+
+    // Codec1 decompresses as much as it can
+    size_t nvalue1 = 0;
+    uint32_t *d_inForCodec2 = d_in;
+
+    if (firstCodecComprLength){
+        nvalue1 = nvalue; // set capacity for codec1 to overall capacity
+
+        d_inForCodec2 = codec1.decodeArrayParallel(d_in, firstCodecComprLength, d_out, nvalue1);
         __syncthreads();
 
-        if (nvalue1 > nvalue){ // Codec1 does not have enough capacity
-            nvalue = nvalue1; // Set capacity to decompressed length
+        if (nvalue1 > nvalue){ // Error - Codec1 does not have enough capacity
+            nvalue = nvalue1; // Set nvalue to required capacity of codec1
             return d_in; // Return pointer to the deginning of the compressed array
         }
+
+        if (d_inForCodec2 == d_in + comprLength - 1){ // Codec1 decompressed everything
+            nvalue = nvalue1;
+            return d_inForCodec2;
+        }
     }
- 
-    if (d_in + length == d_in2) { // Codec 1 did decompress everything
-        nvalue = nvalue1; // Set capacity to decompressed length
-        assert(initin + length >= d_in2);
-        return d_in2; // Return pointer to the end of the compressed array
-    }
+
+    assert(d_inForCodec2 == d_in + firstCodecComprLength); // Make sure codec1 returned correct d_in pointer
 
     // Codec2 decompresses the leftover
-    assert(nvalue > nvalue1);
-    size_t nvalue2 = nvalue - nvalue1;
-    uint32_t *in3 = codec2.decodeArrayParallel(d_in2, length - (d_in2 - d_in), d_out + nvalue1, nvalue2);
+    size_t nvalue2 = nvalue - nvalue1; // remaining capacity
+    size_t leftoverLength = comprLength - 1 - firstCodecComprLength;
+    uint32_t *d_inAfterBothCodecs = codec2.decodeArrayParallel(d_inForCodec2, leftoverLength, d_out + nvalue1, nvalue2);
     __syncthreads();
 
-    if (nvalue2 > nvalue - nvalue1){ // Codec2 does not have enough capacity
-        nvalue = nvalue1 + nvalue2;
-        return d_in;
+    if (nvalue2 > nvalue - nvalue1){ // Error - Codec2 does not have enough capacity
+        nvalue = nvalue1 + nvalue2; // Set nvalue to required capacity of codec1 + codec2
+        return d_in; // Return pointer to the deginning of the compressed array
     }
 
+    assert(d_in + comprLength - 1 == d_inAfterBothCodecs);
     nvalue = nvalue1 + nvalue2;
-    assert(initin + length >= in3);
-    return in3;
+    return d_inAfterBothCodecs;
 }
 
