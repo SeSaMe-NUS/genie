42f13bd7381b7dd8cc219f9c09451a671f66f7b7
Siyuan Liu <weifengzi2009@gmail.com>
2017-07-05 10:38:53 +0800

    add doc for genie's new interface

--- src/GPUGenie/interface/config.h
+++ src/GPUGenie/interface/config.h
@@ -6,6 +6,9 @@
 
 namespace genie {
 
+/*!
+ * \brief Config class holds all user configurable settings of GENIE
+ */
 class Config {
 	private:
 		uint32_t k_;
@@ -13,27 +16,61 @@ class Config {
 		uint32_t num_of_query_;
 		bool save_to_gpu_ = false;
 		uint8_t gpu_id_ = 0;
-		// helper variables
 		bool k_initialized_ = false;
 		bool query_range_initialized_ = false;
 		bool num_of_query_initialized_ = false;
 	public:
-		uint32_t GetK() const;
-		uint32_t GetQueryRange() const;
-		uint32_t GetNumOfQuery() const;
-		bool GetSaveToGpu() const;
-		uint8_t GetGpuId() const;
-		Config& SetK(const uint32_t k);
-		Config& SetQueryRange(const uint32_t query_range);
-		Config& SetNumOfQuery(const uint32_t num_of_query);
-		Config& SetSaveToGpu(const bool save_to_gpu);
-		Config& SetGpuId(const uint8_t gpu_id);
-		Config& LoadFromFile(const std::string& filename);
-		// helper functions
-		bool IsKSet() const;
-		bool IsQueryRangeSet() const;
-		bool IsNumOfQuerySet() const;
-		void Validate() const;
+		/*!
+		 * \brief Return K.
+		 */
+		uint32_t GetK();
+		/*!
+		 * \brief Return the range used in range-based search.
+		 */
+		uint32_t GetQueryRange();
+		/*!
+		 * \brief Return the number of query to search.
+		 */
+		uint32_t GetNumOfQuery();
+		/*!
+		 * \brief Return whether table should be saved to GPU after search.
+		 */
+		bool GetSaveToGpu();
+		/*!
+		 * \brief Return the ID of the GPU used.
+		 */
+		uint8_t GetGpuId();
+		/*!
+		 * \brief Set k.
+		 */
+		Config& SetK(uint32_t k);
+		/*!
+		 * \brief Set the range used in range-based search.
+		 */
+		Config& SetQueryRange(uint32_t query_range);
+		/*!
+		 * \brief Set the number of query to search.
+		 */
+		Config& SetNumOfQuery(uint32_t num_of_query);
+		/*!
+		 * \brief Set whether table should be saved to GPU after search.
+		 */
+		Config& SetSaveToGpu(bool save_to_gpu);
+		/*!
+		 * \brief Set the ID of the GPU used.
+		 */
+		Config& SetGpuId(uint8_t gpu_id);
+		/*!
+		 * \brief Load configuration from a file.
+		 */
+		Config& LoadFromFile(std::string& filename);
+		bool IsKSet();
+		bool IsQueryRangeSet();
+		bool IsNumOfQuerySet();
+		void Validate();
+		/*!
+		 * \brief Display enabled configuration options.
+		 */
 		void DisplayConfiguration();
 };
 
